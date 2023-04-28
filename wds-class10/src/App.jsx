8195b98b5c8f5fc6d522e68bbff0e3c932b8549e
MartinMSalas <ingmartinmsalas@gmail.com>
2023-04-28 12:27:22 -0300

    adding example

--- wds-class10/src/App.jsx
+++ wds-class10/src/App.jsx
@@ -5,16 +5,22 @@ import './App.css'
 import { TodoList } from './components/TodoList'
 import { Name } from './components/Name'
 import { NameFunc } from './components/NameFunc'
+import { TodoListItem } from './components/TodoListItem'
 
 
 function App() {
  
   return (
     <>
-      <NameFunc name="Agent 1" age={1}></NameFunc>
+      <TodoListItem isComplete={True}>
+        Todo item 1
+      </TodoListItem>
+
+
+      {/* <NameFunc name="Agent 1" age={1}></NameFunc>
       <NameFunc name="Agent 4" age={7}></NameFunc>
       <h1>Todo List</h1>
-      <TodoList></TodoList>
+      <TodoList></TodoList> */}
       
     </>
   )
---
+++ wds-class10/src/components/TodoListItem.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export function TodoListItem({children, isComplete}){
+   
+    return(
+        <div>
+            <label htmlFor="checkb">input onClick={changeCheckState} value={showCheckState} id="checkb" type="checkbox"></input>
+            </label>
+           
+        </div>
+        
+    ) 
+}
